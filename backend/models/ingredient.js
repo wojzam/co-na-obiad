6a71e442cf6c898f2883fbdaefbd478a23f6887a
wojzam <wojciech.zamarski@student.pk.edu.pl>
2024-08-01 21:25:06 +0200

    Remove type from ingredient

--- backend/models/ingredient.js
+++ backend/models/ingredient.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const ingredientSchema = new mongoose.Schema({
     _id: {type: mongoose.Schema.Types.ObjectId, required: true},
     name: {type: String, required: true},
-    type: {
-        _id: {type: mongoose.Schema.Types.ObjectId, ref: 'IngredientType', required: true},
-        name: {type: String, required: true}
-    },
 });
 
 ingredientSchema.index({name: 1});
