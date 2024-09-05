95c27ac4e43226ff2cd9e82f6f98a1c22485220a
wojzam <wojciech.zamarski@student.pk.edu.pl>
2024-09-05 18:24:30 +0200

    Add colors to categories

--- frontend/src/components/CategoryLink.jsx
+++ frontend/src/components/CategoryLink.jsx
@@ -14,15 +14,26 @@ const CategoryLink = ({category}) => {
         window.location.href = path;
     };
 
+    const COLOR_LIGHTNESS = theme.palette.mode === "dark" ? 40 : 80;
+    const generateColor = (saturation = 18, lightness = COLOR_LIGHTNESS) => {
+        const str = category.name.trim() + category.name.trim();
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        return `hsl(${hash % 360}, ${saturation}%, ${lightness}%)`;
+    }
+
     return (
         <Typography variant="subtitle2" fontWeight="medium" onClick={onClick} gutterBottom sx={{
             display: 'inline-block',
-            backgroundColor: theme.palette.lightGrey.main,
+            backgroundColor: generateColor(),
             borderRadius: "5px",
             px: 1.5,
             whiteSpace: "nowrap",
             "&:hover": {
-                background: theme.palette.neutral.darker,
+                background: generateColor(20, COLOR_LIGHTNESS - 10),
                 boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.30)",
                 cursor: "pointer"
             },
